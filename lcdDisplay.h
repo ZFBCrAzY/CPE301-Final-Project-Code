ccdb58caf7bd6ed811b9262790508c79b916a6b5
iCheese22 <89930243+iCheese22@users.noreply.github.com>
2026-04-29 01:42:09 -0700

    Update lcdDisplay.h

--- lcdDisplay.h
+++ lcdDisplay.h
@@ -9,8 +9,14 @@
 #ifndef LCDDISPLAY_H_
 #define LCDDISPLAY_H_
 
+#include <avr/io.h>
+#include <stdint.h>
 
-
-
+void lcd_init(void);
+void lcd_command(uint8_t cmd);
+void lcd_data(uint8_t data);
+void lcd_print(const char *str);
+void lcd_clear(void);
+void lcd_gotoxy(uint8_t col, uint8_t row);
 
 #endif /* LCDDISPLAY_H_ */
